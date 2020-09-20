93dbec02f7aaba6f14dc37a37ad25f6efd68eb5a
Rhaysa Madruga <rysamad@gmail.com>
2020-09-20 12:12:02 -0300

    Base and animations

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rescue-game
+
+<p>Learn HTML, CSS, JS by making games.</p>
+<p>A project developed for JavaScript Game Developer Bootcamp at Digital Innovation One.</p>
---
+++ js/script.js
@@ -0,0 +1,8 @@
+function start() {
+  $("#start").hide();
+
+  $("#background-game").append("<div id='player' class='animation-player'></div>");
+  $("#background-game").append("<div id='enemy1' class='animation-enemy'></div>");
+  $("#background-game").append("<div id='enemy2' class='animation-enemy2'></div>");
+  $("#background-game").append("<div id='ally' class='animation-ally'></div>");
+}
