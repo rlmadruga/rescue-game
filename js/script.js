db64d2eafd04eee73695407339adecce0b9dba14
Rhaysa Madruga <rysamad@gmail.com>
2020-09-20 12:47:45 -0300

    Moving background

--- js/script.js
+++ js/script.js
@@ -5,4 +5,17 @@ function start() {
   $("#background-game").append("<div id='enemy1' class='animation-enemy'></div>");
   $("#background-game").append("<div id='enemy2' class='animation-enemy2'></div>");
   $("#background-game").append("<div id='ally' class='animation-ally'></div>");
+
+  let game = {};
+
+  game.timer = setInterval(loop, 30);
+
+  function loop() {
+    moveBackground();
+  }
+
+  function moveBackground() {
+    moveLeft = parseInt($("#background-game").css("background-position"));
+    $("#background-game").css("background-position", moveLeft - 1);
+  }
 }
